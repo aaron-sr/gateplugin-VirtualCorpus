9bc90e03d6b02316743a2ac488259b0cbea72182
aaron-sr <aaron.schlutter@tu-berlin.de>
2019-04-04 19:45:56 +0200

    add serialization format and exporter
    - documents can be stored via byte[] or streams
    - you need to use an encoding like ISO-8859-1 which has a 1-to-1 mapping
    between bytes and characters, because GATE wants to load a document with
    string content and SerializationFormat will map it back to byte[]

---
+++ src/main/java/gate/format/SerializationExporter.java
@@ -0,0 +1,29 @@
+package gate.format;
+
+import java.io.IOException;
+import java.io.ObjectOutputStream;
+import java.io.OutputStream;
+
+import org.apache.log4j.Logger;
+
+import gate.Document;
+import gate.DocumentExporter;
+import gate.FeatureMap;
+import gate.creole.metadata.AutoInstance;
+import gate.creole.metadata.CreoleResource;
+
+@CreoleResource(name = "Serialization Exporter", tool = true, autoinstances = @AutoInstance)
+public class SerializationExporter extends DocumentExporter {
+	private static final long serialVersionUID = -1437154772909153534L;
+	private static Logger logger = Logger.getLogger(SerializationExporter.class);
+
+	public SerializationExporter() {
+		super("Java Serialization", "ser", "application/java-serialized-object");
+	}
+
+	@Override
+	public void export(Document document, OutputStream outputStream, FeatureMap options) throws IOException {
+		new ObjectOutputStream(outputStream).writeObject(document);
+	}
+
+}
---
+++ src/main/java/gate/format/SerializationFormat.java
@@ -0,0 +1,126 @@
+package gate.format;
+
+import java.io.ByteArrayInputStream;
+import java.io.ObjectInputStream;
+import java.util.ArrayList;
+import java.util.Collection;
+
+import org.apache.log4j.Logger;
+
+import gate.Annotation;
+import gate.Document;
+import gate.DocumentFormat;
+import gate.Resource;
+import gate.TextualDocument;
+import gate.corpora.DocumentContentImpl;
+import gate.corpora.MimeType;
+import gate.corpora.RepositioningInfo;
+import gate.creole.ResourceInstantiationException;
+import gate.creole.metadata.AutoInstance;
+import gate.creole.metadata.CreoleResource;
+import gate.util.DocumentFormatException;
+
+@CreoleResource(name = "Serialization Format", isPrivate = true, autoinstances = { @AutoInstance(hidden = true) })
+public class SerializationFormat extends DocumentFormat {
+	private static final long serialVersionUID = 7836956734599945213L;
+	private static Logger logger = Logger.getLogger(SerializationFormat.class);
+
+	@Override
+	public void unpackMarkup(Document document) throws DocumentFormatException {
+
+		try {
+			byte[] bytes;
+			if (document instanceof TextualDocument) {
+				bytes = document.getContent().toString().getBytes(((TextualDocument) document).getEncoding());
+			} else {
+				bytes = document.getContent().toString().getBytes();
+			}
+			if (bytes.length > 0) {
+				try (ObjectInputStream objectInputStream = new ObjectInputStream(new ByteArrayInputStream(bytes))) {
+					Document readDocument = (Document) objectInputStream.readObject();
+					validateEmptyDocument(document);
+					applyDocumentValues(readDocument, document);
+				}
+			}
+		} catch (Exception e) {
+			throw new DocumentFormatException(e);
+		}
+	}
+
+	private void validateEmptyDocument(Document document) throws DocumentFormatException {
+		if (!document.getAnnotations().isEmpty()) {
+			throw new DocumentFormatException("document has already annotations in default annotation set");
+		}
+		if (!document.getAnnotations().getRelations().isEmpty()) {
+			throw new DocumentFormatException("document has already relations in default annotation set");
+		}
+		Collection<String> annotationSetNames = new ArrayList<>();
+		Collection<String> relationSetNames = new ArrayList<>();
+		for (String annotationSetName : document.getAnnotationSetNames()) {
+			if (annotationSetName.length() == 0) {
+				continue;
+			}
+			if (!document.getAnnotations(annotationSetName).isEmpty()) {
+				annotationSetNames.add(annotationSetName);
+			}
+			if (!document.getAnnotations(annotationSetName).getRelations().isEmpty()) {
+				relationSetNames.add(annotationSetName);
+			}
+		}
+		if (!annotationSetNames.isEmpty() && !relationSetNames.isEmpty()) {
+			throw new DocumentFormatException("document has already annotations in " + annotationSetNames
+					+ " and relations in " + relationSetNames);
+		} else if (!annotationSetNames.isEmpty()) {
+			throw new DocumentFormatException("document has already annotations in " + annotationSetNames);
+		} else if (!relationSetNames.isEmpty()) {
+			throw new DocumentFormatException("document has already relations in " + relationSetNames);
+		}
+	}
+
+	@Override
+	public void unpackMarkup(Document document, RepositioningInfo repInfo, RepositioningInfo ampCodingInfo)
+			throws DocumentFormatException {
+		unpackMarkup(document);
+	}
+
+	@Override
+	public Resource init() throws ResourceInstantiationException {
+		MimeType mime = new MimeType("application", "java-serialized-object");
+		mimeString2ClassHandlerMap.put(mime.getType() + "/" + mime.getSubtype(), this);
+		mimeString2mimeTypeMap.put(mime.getType() + "/" + mime.getSubtype(), mime);
+		suffixes2mimeTypeMap.put("ser", mime);
+//		magic2mimeTypeMap.put("<?xml", mime);
+		setMimeType(mime);
+		return this;
+	}
+
+	public static final void applyDocumentValues(Document fromDocument, Document toDocument) {
+		toDocument.setContent(new DocumentContentImpl(fromDocument.getContent().toString()));
+		if (!fromDocument.getAnnotations().isEmpty()) {
+			for (Annotation annotation : fromDocument.getAnnotations()) {
+				toDocument.getAnnotations().add(annotation);
+			}
+		}
+		if (!fromDocument.getAnnotations().getRelations().isEmpty()) {
+			toDocument.getAnnotations().getRelations().addAll(fromDocument.getAnnotations().getRelations());
+		}
+		for (String annotationSetName : fromDocument.getAnnotationSetNames()) {
+			if (annotationSetName.length() == 0) {
+				continue;
+			}
+			if (!fromDocument.getAnnotations(annotationSetName).isEmpty()) {
+				for (Annotation annotation : fromDocument.getAnnotations(annotationSetName)) {
+					toDocument.getAnnotations(annotationSetName).add(annotation);
+				}
+			}
+			if (!fromDocument.getAnnotations(annotationSetName).getRelations().isEmpty()) {
+				toDocument.getAnnotations(annotationSetName).getRelations()
+						.addAll(fromDocument.getAnnotations(annotationSetName).getRelations());
+			}
+		}
+		if (!fromDocument.getFeatures().isEmpty()) {
+			toDocument.getFeatures().putAll(fromDocument.getFeatures());
+		}
+	}
+
+}
