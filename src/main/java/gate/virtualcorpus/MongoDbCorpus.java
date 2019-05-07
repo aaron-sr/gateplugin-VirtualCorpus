b89b5cf2ce9d45836d00e867a0be5b3d5dfed837
aaron-sr <aaron.schlutter@tu-berlin.de>
2019-05-07 17:08:21 +0200

    improved mongodb corpus
    only load necessary keys instead of whole document
    export to separate keys (like jdbc)
    optional id cache to avoid cursor skip
    batch size as gate parameter

--- src/main/java/gate/virtualcorpus/MongoDbCorpus.java
+++ src/main/java/gate/virtualcorpus/MongoDbCorpus.java
@@ -1,9 +1,17 @@
 package gate.virtualcorpus;
 
+import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.apache.log4j.Logger;
+import org.bson.types.ObjectId;
+import org.mapdb.DB;
+import org.mapdb.DBMaker;
+import org.mapdb.Serializer;
 
 import com.mongodb.MongoClient;
 import com.mongodb.MongoClientOptions;
@@ -11,7 +19,12 @@ import com.mongodb.MongoCredential;
 import com.mongodb.ServerAddress;
 import com.mongodb.client.FindIterable;
 import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoCursor;
 import com.mongodb.client.MongoDatabase;
+import com.mongodb.client.model.Filters;
+import com.mongodb.client.model.Projections;
+import com.mongodb.client.model.Sorts;
+import com.mongodb.client.model.Updates;
 
 import gate.Corpus;
 import gate.Document;
@@ -22,6 +35,7 @@ import gate.corpora.DocumentImpl;
 import gate.creole.ResourceInstantiationException;
 import gate.creole.metadata.CreoleParameter;
 import gate.creole.metadata.CreoleResource;
+import gate.creole.metadata.Optional;
 import gate.util.GateRuntimeException;
 
 @CreoleResource(name = "MongoDbCorpus", interfaceName = "gate.Corpus", icon = "corpus", comment = "A corpus backed by GATE documents stored in a MongoDB")
@@ -31,23 +45,32 @@ public class MongoDbCorpus extends VirtualCorpus implements Corpus {
 
 	public static final String ID_KEY_NAME = "_id";
 
-	private String host;
-	private Integer port;
-	private String userName;
-	private String password;
-	private String databaseName;
-	private String collectionName;
-	private String contentKeys;
-	private String featureKeys;
+	protected String host;
+	protected Integer port;
+	protected String userName;
+	protected String password;
+	protected String databaseName;
+	protected String collectionName;
+	protected String contentKeys;
+	protected String featureKeys;
+	protected String exportKeySuffix;
+	protected String exportEncoding;
+	protected Integer batchSize;
+	protected Boolean cacheIds;
+
+	private List<String> contentKeyList;
+	private List<String> featureKeyList;
+	private Map<String, String> exportKeyMapping;
 
 	private MongoClient client;
 	private MongoDatabase database;
 	private MongoCollection<org.bson.Document> collection;
 	private FindIterable<org.bson.Document> cursor;
 	private int cursorPosition;
-
-	private List<String> contentKeyList;
-	private List<String> featureKeyList;
+	private Map<Integer, String> idCache;
+	private MongoCursor<org.bson.Document> iterator;
+	private int iteratorPosition;
+	private DB cacheDb;
 
 	@CreoleParameter(comment = "The host of the MongoDB", defaultValue = "localhost")
 	public void setHost(String host) {
@@ -121,6 +144,46 @@ public class MongoDbCorpus extends VirtualCorpus implements Corpus {
 		return featureKeys;
 	}
 
+	@Optional
+	@CreoleParameter(comment = "suffix for value keys, where exported data is written", defaultValue = "")
+	public void setExportKeySuffix(String exportKeySuffix) {
+		this.exportKeySuffix = exportKeySuffix;
+	}
+
+	public String getExportKeySuffix() {
+		return exportKeySuffix;
+	}
+
+	@Optional
+	@CreoleParameter(comment = "encoding for value keys, which were exported (in case of reopen document)", defaultValue = "")
+	public void setExportEncoding(String exportEncoding) {
+		this.exportEncoding = exportEncoding;
+	}
+
+	public String getExportEncoding() {
+		return exportEncoding;
+	}
+
+	@Optional
+	@CreoleParameter(comment = "batch size of mongodb cursor", defaultValue = "")
+	public void setBatchSize(Integer batchSize) {
+		this.batchSize = batchSize;
+	}
+
+	public Integer getBatchSize() {
+		return batchSize;
+	}
+
+	@Optional
+	@CreoleParameter(comment = "cache ids to reduce cursor skip", defaultValue = "false")
+	public void setCacheIds(Boolean cacheIds) {
+		this.cacheIds = cacheIds;
+	}
+
+	public Boolean getCacheIds() {
+		return cacheIds;
+	}
+
 	@Override
 	public Resource init() throws ResourceInstantiationException {
 		checkValidMimeType();
@@ -155,9 +218,27 @@ public class MongoDbCorpus extends VirtualCorpus implements Corpus {
 		collection = database.getCollection(collectionName);
 		contentKeyList = splitUserInput(this.contentKeys);
 		featureKeyList = splitUserInput(this.featureKeys);
+		if (hasValue(exportKeySuffix)) {
+			exportKeyMapping = new HashMap<>();
+			for (String contentKey : contentKeyList) {
+				String exportKey = contentKey + exportKeySuffix;
+				exportKeyMapping.put(contentKey, exportKey);
+			}
+		}
 
-		cursor = collection.find().sort(null);
+		cursor = collection.find().noCursorTimeout(true).sort(Sorts.ascending(ID_KEY_NAME));
+		if (batchSize != null) {
+			cursor = cursor.batchSize(batchSize);
+		}
 		cursorPosition = 0;
+
+		if (cacheIds) {
+			cursor = cursor.projection(Projections.include(ID_KEY_NAME));
+			iterator = cursor.iterator();
+			cacheDb = DBMaker.tempFileDB().fileMmapEnableIfSupported().fileMmapPreclearDisable().cleanerHackEnable()
+					.fileChannelEnable().make();
+			idCache = cacheDb.hashMap(name, Serializer.INTEGER, Serializer.STRING).createOrOpen();
+		}
 
 		initVirtualCorpus();
 
@@ -166,8 +247,14 @@ public class MongoDbCorpus extends VirtualCorpus implements Corpus {
 
 	@Override
 	public void cleanup() {
+		if (iterator != null) {
+			iterator.close();
+		}
 		if (client != null) {
 			client.close();
+		}
+		if (cacheDb != null) {
+			cacheDb.close();
 		}
 	}
 
@@ -190,8 +277,7 @@ public class MongoDbCorpus extends VirtualCorpus implements Corpus {
 		Integer documentIndex = documentIndex(index);
 		String contentKey = contentKey(index);
 
-		org.bson.Document document = getDocument(documentIndex);
-		Object id = document.get(ID_KEY_NAME);
+		Object id = getId(documentIndex);
 
 		return buildDocumentName(id, contentKey);
 	}
@@ -201,12 +287,33 @@ public class MongoDbCorpus extends VirtualCorpus implements Corpus {
 		Integer documentIndex = documentIndex(index);
 		String contentKey = contentKey(index);
 
-		org.bson.Document mongoDbDocument = getDocument(documentIndex);
-		Object content = mongoDbDocument.get(contentKey);
+		List<String> includeKeys = new ArrayList<>();
+		includeKeys.add(contentKey);
+		includeKeys.addAll(featureKeyList);
+		if (hasValue(exportKeySuffix)) {
+			String exportKey = exportKeyMapping.get(contentKey);
+			includeKeys.add(exportKey);
+		}
+		org.bson.Document mongoDbDocument = getDocument(documentIndex, includeKeys);
+
+		Object content = null;
+		String encoding = null;
+		String mimeType = null;
+		if (hasValue(exportKeySuffix)) {
+			String exportKey = exportKeyMapping.get(contentKey);
+			content = mongoDbDocument.get(exportKey);
+			encoding = exportEncoding;
+			mimeType = getExporter().getMimeType();
+		}
+		if (content == null) {
+			content = mongoDbDocument.get(contentKey);
+			encoding = this.encoding;
+			mimeType = this.mimeType;
+		}
 		if (content == null) {
 			content = "";
-		} else if (content instanceof byte[]) {
-			content = new String((byte[]) content, encoding);
+		} else if (content instanceof org.bson.types.Binary) {
+			content = new String(((org.bson.types.Binary) content).getData(), encoding);
 		} else if (!(content instanceof String)) {
 			content = content.toString();
 		}
@@ -233,9 +340,14 @@ public class MongoDbCorpus extends VirtualCorpus implements Corpus {
 		Integer documentIndex = documentIndex(index);
 		String contentKey = contentKey(index);
 
-		org.bson.Document mongoDbDocument = getDocument(documentIndex);
+		if (hasValue(exportKeySuffix)) {
+			contentKey = exportKeyMapping.get(contentKey);
+		}
+		byte[] bytes = export(getExporter(), document);
 
-		mongoDbDocument.put(contentKey, export(getExporter(), document));
+		String id = getId(documentIndex);
+
+		collection.updateOne(Filters.eq(ID_KEY_NAME, new ObjectId(id)), Updates.set(contentKey, bytes));
 	}
 
 	@Override
@@ -248,13 +360,39 @@ public class MongoDbCorpus extends VirtualCorpus implements Corpus {
 		throw new UnsupportedOperationException();
 	}
 
-	private org.bson.Document getDocument(Integer documentIndex) {
+	private String getId(Integer documentIndex) {
+		if (cacheIds) {
+			String id = idCache.get(documentIndex);
+			if (id != null) {
+				return id;
+			}
+			while (iteratorPosition <= documentIndex) {
+				org.bson.Document mongoDbDocument = iterator.next();
+				String next = mongoDbDocument.getObjectId(ID_KEY_NAME).toHexString();
+				idCache.put(iteratorPosition, next);
+				try {
+					if (iteratorPosition == documentIndex) {
+						return next;
+					}
+				} finally {
+					iteratorPosition++;
+				}
+			}
+		}
+		return getDocument(documentIndex, Collections.emptyList()).getObjectId(ID_KEY_NAME).toHexString();
+	}
+
+	private org.bson.Document getDocument(Integer documentIndex, List<String> includeKeys) {
+		if (cacheIds) {
+			String id = getId(documentIndex);
+			return collection.find(Filters.eq(ID_KEY_NAME, new ObjectId(id)))
+					.projection(Projections.include(includeKeys)).first();
+		}
 		if (documentIndex != cursorPosition) {
 			cursor.skip(documentIndex);
 			cursorPosition = documentIndex;
 		}
-		org.bson.Document document = cursor.first();
-		return document;
+		return cursor.projection(Projections.include(includeKeys)).first();
 	}
 
 	private String buildDocumentName(Object id, String contentKey) {
